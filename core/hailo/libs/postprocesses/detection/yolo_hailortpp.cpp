c863cf5a05965a23a52ed22e7a5216dfcc5e3887
HailoRT-Automation <contact@hailo.ai>
2024-07-31 13:29:04 +0300

    v3.29.1

--- core/hailo/libs/postprocesses/detection/yolo_hailortpp.cpp
+++ core/hailo/libs/postprocesses/detection/yolo_hailortpp.cpp
@@ -110,6 +110,90 @@ void free_resources(void *params_void_ptr)
     delete params;
 }
 
+#if __GNUC__ > 8
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#endif
+
+YoloParamsNMS *init(const std::string config_path, const std::string function_name)
+{
+    YoloParamsNMS *params;
+    if (!fs::exists(config_path))
+    {
+        params = new YoloParamsNMS(common::coco_eighty);
+        return params;
+    }
+    else
+    {
+        params = new YoloParamsNMS();
+        char config_buffer[4096];
+        const char *json_schema = R""""({
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "detection_threshold": {
+            "type": "number",
+            "minimum": 0,
+            "maximum": 1
+            },
+            "max_boxes": {
+            "type": "integer"
+            },
+            "labels": {
+            "type": "array",
+            "items": {
+                "type": "string"
+                }
+            }
+        },
+        "required": [
+            "labels"
+        ]
+        })"""";
+
+        std::FILE *fp = fopen(config_path.c_str(), "r");
+        if (fp == nullptr)
+        {
+            throw std::runtime_error("JSON config file is not valid");
+        }
+        rapidjson::FileReadStream stream(fp, config_buffer, sizeof(config_buffer));
+        bool valid = common::validate_json_with_schema(stream, json_schema);
+        if (valid)
+        {
+            rapidjson::Document doc_config_json;
+            doc_config_json.ParseStream(stream);
+
+            // parse labels
+            auto labels = doc_config_json["labels"].GetArray();
+            uint i = 0;
+            for (auto &v : labels)
+            {
+                params->labels.insert(std::pair<std::uint8_t, std::string>(i, v.GetString()));
+                i++;
+            }
+
+            // set the params
+            if (doc_config_json.HasMember("detection_threshold")) {
+                params->detection_threshold = doc_config_json["detection_threshold"].GetFloat();
+            }
+            if (doc_config_json.HasMember("max_boxes")) {
+                params->max_boxes = doc_config_json["max_boxes"].GetInt();
+                params->filter_by_score = true;
+            }
+        }
+        fclose(fp);
+    }
+    return params;
+}
+void free_resources(void *params_void_ptr)
+{
+    YoloParamsNMS *params = reinterpret_cast<YoloParamsNMS *>(params_void_ptr);
+    delete params;
+}
+
 static std::map<uint8_t, std::string> yolo_vehicles_labels = {
     {0, "unlabeled"},
     {1, "car"}};
