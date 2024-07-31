865af686f73ed81b40cc2f3d74b6fbf053341257
HailoRT-Automation <contact@hailo.ai>
2024-07-31 13:29:04 +0300

    v3.29.1

--- core/hailo/libs/postprocesses/detection/yolo_hailortpp.cpp
+++ core/hailo/libs/postprocesses/detection/yolo_hailortpp.cpp
@@ -1,14 +1,17 @@
 #include <regex>
+#include <fstream>
+#include <sstream>
+#include <map>
 #include "rapidjson/document.h"
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/error/en.h"
 #include "rapidjson/filereadstream.h"
 #include "rapidjson/schema.h"
 #include "json_config.hpp"
-#include "hailo_nms_decode.hpp"
-#include "yolo_hailortpp.hpp"
 #include "common/labels/coco_eighty.hpp"
 #include "common/labels/yolo_personface.hpp"
+#include "hailo_nms_decode.hpp"
+#include "yolo_hailortpp.hpp"
 
 static const std::string DEFAULT_YOLOV5S_OUTPUT_LAYER = "yolov5s_nv12/yolov5_nms_postprocess";
 static const std::string DEFAULT_YOLOV5M_OUTPUT_LAYER = "yolov5m_wo_spp_60p/yolov5_nms_postprocess";
@@ -95,6 +98,90 @@ YoloParamsNMS *init(const std::string config_path, const std::string function_na
             }
             if (doc_config_json.HasMember("output_layer_name")) {
                 params->output_layer_name = doc_config_json["output_layer_name"].GetString();
+            }
+        }
+        fclose(fp);
+    }
+    return params;
+}
+void free_resources(void *params_void_ptr)
+{
+    YoloParamsNMS *params = reinterpret_cast<YoloParamsNMS *>(params_void_ptr);
+    delete params;
+}
+
+#if __GNUC__ > 8
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#endif
+
+YoloParamsNMS *init(const std::string config_path, const std::string function_name)
+{
+    YoloParamsNMS *params;
+    if (!fs::exists(config_path))
+    {
+        params = new YoloParamsNMS(common::coco_eighty);
+        return params;
+    }
+    else
+    {
+        params = new YoloParamsNMS();
+        char config_buffer[4096];
+        const char *json_schema = R""""({
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "detection_threshold": {
+            "type": "number",
+            "minimum": 0,
+            "maximum": 1
+            },
+            "max_boxes": {
+            "type": "integer"
+            },
+            "labels": {
+            "type": "array",
+            "items": {
+                "type": "string"
+                }
+            }
+        },
+        "required": [
+            "labels"
+        ]
+        })"""";
+
+        std::FILE *fp = fopen(config_path.c_str(), "r");
+        if (fp == nullptr)
+        {
+            throw std::runtime_error("JSON config file is not valid");
+        }
+        rapidjson::FileReadStream stream(fp, config_buffer, sizeof(config_buffer));
+        bool valid = common::validate_json_with_schema(stream, json_schema);
+        if (valid)
+        {
+            rapidjson::Document doc_config_json;
+            doc_config_json.ParseStream(stream);
+
+            // parse labels
+            auto labels = doc_config_json["labels"].GetArray();
+            uint i = 0;
+            for (auto &v : labels)
+            {
+                params->labels.insert(std::pair<std::uint8_t, std::string>(i, v.GetString()));
+                i++;
+            }
+
+            // set the params
+            if (doc_config_json.HasMember("detection_threshold")) {
+                params->detection_threshold = doc_config_json["detection_threshold"].GetFloat();
+            }
+            if (doc_config_json.HasMember("max_boxes")) {
+                params->max_boxes = doc_config_json["max_boxes"].GetInt();
+                params->filter_by_score = true;
             }
         }
         fclose(fp);
