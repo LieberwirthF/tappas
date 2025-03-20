c72be8b45f1f8977cabbd045bf958e25cc407bb6
Fridtjof <lieberwirth@dresearch-fe.de>
2025-03-20 10:36:36 +0100

    added yolov7 support to yolo_hailortpp.cpp

--- core/hailo/libs/postprocesses/detection/yolo_hailortpp.hpp
+++ core/hailo/libs/postprocesses/detection/yolo_hailortpp.hpp
@@ -6,11 +6,36 @@
 #include "hailo_objects.hpp"
 #include "hailo_common.hpp"
 
-
 __BEGIN_DECLS
-void filter(HailoROIPtr roi);
+
+class YoloParamsNMS
+{
+public:
+    std::map<std::uint8_t, std::string> labels;
+    float detection_threshold;
+    uint max_boxes;
+    bool filter_by_score=false;
+    std::string output_layer_name;
+    YoloParamsNMS(std::map<uint8_t, std::string> dataset = std::map<uint8_t, std::string>(),
+                  float detection_threshold = 0.3f,
+                  uint max_boxes = 200)
+        : labels(dataset),
+          detection_threshold(detection_threshold), 
+          max_boxes(max_boxes) {}
+};
+
+YoloParamsNMS *init(const std::string config_path, const std::string function_name);
+void free_resources(void *params_void_ptr);
+void filter(HailoROIPtr roi, void *params_void_ptr);
+void filter_letterbox(HailoROIPtr roi, void *params_void_ptr);
 void yolov5(HailoROIPtr roi);
+void yolov5s_nv12(HailoROIPtr roi);
+void yolov7tiny(HailoROIPtr roi, void *params_void_ptr);
+void yolov8s(HailoROIPtr roi);
+void yolov8m(HailoROIPtr roi);
 void yolox(HailoROIPtr roi);
+void yolov5s_personface(HailoROIPtr roi);
 void yolov5_no_persons(HailoROIPtr roi);
 void yolov5m_vehicles(HailoROIPtr roi);
+void yolov5m_vehicles_nv12(HailoROIPtr roi);
 __END_DECLS
